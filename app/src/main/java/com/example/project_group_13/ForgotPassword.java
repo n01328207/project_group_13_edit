71bd562d35235656d81748eec2c5529896144c63
XuanHaMinhNghiem <n01328207@humbermail.ca>
2020-11-18 18:39:43 -0500

    added back button

--- app/src/main/java/com/example/project_group_13/ForgotPassword.java
+++ app/src/main/java/com/example/project_group_13/ForgotPassword.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ForgotPassword extends AppCompatActivity {
     Button forgot;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +24,14 @@ public class ForgotPassword extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        back = (ImageView) findViewById(R.id.imageView14);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(ForgotPassword.this,SignIn.class);
+            startActivity(intent);
+        }
+    });
+}
 }
